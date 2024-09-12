e2b42c9e89dc8a7ea565882432387c28c90d0204
joonyoung1 <joonyoung1023@gmail.com>
2024-09-12 16:44:22 +0900

    Partially implement disassembler - need to fully implement instructions that communicate with keypad and display - need to implement 0xF--- instructions

--- chip8.cpp
+++ chip8.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <random>
 
 class Chip8 {
 public:
@@ -52,11 +53,14 @@ public:
 
     void run() {
         uint16_t opcode;
-        uint16_t highNibble;
         uint16_t addr;
         uint8_t x;
         uint8_t y;
+        uint8_t n;
         uint8_t kk;
+
+        std::mt19937 rng(std::random_device{}());
+        std::uniform_int_distribution<int> dist(0, 255);
 
         for (;;) {
             opcode = memory[pc];
@@ -64,48 +68,230 @@ public:
             opcode |= memory[pc + 1];
 
             switch (opcode & 0xF000) {
-                case 0x0000:
-                    if (opcode == 0x00E0) { // 00E0 CLS
-                        display.reset();
-                        pc += 2;
-                    } else if (opcode == 0x00EE) { // 00EE RET
-                        pc = stack[sp];
-                        sp--;
-                        pc += 2;
-                    }
+            case 0x0000:
+                switch (opcode) {
+                case 0x00E0: // 00E0 - CLS
+                    display.reset();
+                    pc += 2;
                     break;
 
-                case 0x1000: // 1nnn - P addr
-                    addr = opcode & 0x0FFF;
-                    pc = addr;
+                case 0x00EE: // ooEE - RET
+                    pc = stack[sp];
+                    sp--;
+                    pc += 2;
                     break;
 
-                case 0x2000: // 2nnn - CALL addr
-                    addr = opcode & 0x0FFF;
-                    sp++;
-                    stack[sp] = pc;
-                    pc = addr;
+                default:
+                    pc += 2;
                     break;
+                }
+                break;
 
-                case 0x3000: // 3xkk - SE Vx, byte
+            case 0x1000: // 1nnn - P addr
+                addr = opcode & 0x0FFF;
+                pc = addr;
+                break;
+
+            case 0x2000: // 2nnn - CALL addr
+                addr = opcode & 0x0FFF;
+                sp++;
+                stack[sp] = pc;
+                pc = addr;
+                break;
+
+            case 0x3000: // 3xkk - SE Vx, byte
+                x = (opcode & 0x0F00) >> 8;
+                kk = opcode & 0x00FF;
+                pc += (V[x] == kk) ? 4 : 2;
+                break;
+
+            case 0x4000: // 4xkk - SNE Vx, byte
+                x = (opcode & 0x0F00) >> 8;
+                kk = opcode & 0X00FF;
+                pc += (V[x] != kk) ? 4 : 2;
+                break;
+
+            case 0x5000: // 5xy0 - SE Vx, Vy
+                if ((opcode & 0x000F) == 0x0000) {
                     x = (opcode & 0x0F00) >> 8;
-                    kk = opcode & 0x00FF;
-                    pc += (V[x] == kk) ? 4 : 2;
-                    break;
+                    y = (opcode & 0x00F0) >> 4;
+                    pc += (V[x] == V[y]) ? 4 : 2;
+                }
+                break;
 
-                case 0x4000: // 4xkk - SNE Vx, byte
+            case 0x6000: // 6xkk - LD Vx, byte
+                x = (opcode & 0x0F00) >> 8;
+                kk = opcode & 0x00FF;
+                V[x] = kk;
+                pc += 2;
+                break;
+
+            case 0x7000: // 7xkk - ADD Vx, byte
+                x = (opcode & 0x0F00) >> 8;
+                kk = opcode & 0x00FF;
+                V[x] += kk;
+                pc += 2;
+                break;
+
+            case 0x8000:
+                switch (opcode & 0x000F) {
+                case 0x0000: // 8xy0 - LD Vx, Vy
                     x = (opcode & 0x0F00) >> 8;
-                    kk = opcode & 0X00FF;
-                    pc += (V[x] != kk) ? 4 : 2;
+                    y = (opcode & 0x00F0) >> 4;
+                    V[x] = V[y];
+                    pc += 2;
                     break;
 
-                case 0x5000:
-                    if ((opcode & 0x000F) == 0x0000) {
-                        x = (opcode & 0x0F00) >> 8;
-                        y = (opcode & 0x00F0) >> 4;
-                        pc += (V[x] == V[y]) ? 4 : 2;
-                    }
+                case 0x0001: // 8xy1 - OR Vx, Vy
+                    x = (opcode & 0x0F00) >> 8;
+                    y = (opcode & 0x00F0) >> 4;
+                    V[x] |= V[y];
+                    pc += 2;
                     break;
+
+                case 0x0002: // 8xy2 - AND Vx, Vy
+                    x = (opcode & 0x0F00) >> 8;
+                    y = (opcode & 0x00F0) >> 4;
+                    V[x] &= V[y];
+                    pc += 2;
+                    break;
+
+                case 0x0003: // 8xy3 - XOR Vx, Vy
+                    x = (opcode & 0x0F00) >> 8;
+                    y = (opcode & 0x00F0) >> 4;
+                    V[x] ^= V[y];
+                    pc += 2;
+                    break;
+
+                case 0x0004: // 8xy4 - ADD Vx, Vy
+                    V[x] += V[y];
+                    V[0xF] = (V[x] < V[y]) ? 1 : 0;
+                    pc += 2;
+                    break;
+
+                case 0x0005: // 8xy5 - SUB Vx, Vy
+                    x = (opcode & 0x0F00) >> 8;
+                    y = (opcode & 0x00F0) >> 4;
+                    V[0xF] = (V[x] > V[y]) ? 1 : 0;
+                    V[x] -= V[y];
+                    pc += 2;
+                    break;
+
+                case 0x0006: // 8xy6 - SHR Vx {, Vy}
+                    x = (opcode & 0x0F00) >> 8;
+                    V[0xF] = V[x] & 0x1;
+                    V[x] >>= 1;
+                    pc += 2;
+                    break;
+
+                case 0x0007: // 8xy7 - SUBN Vx {, Vy}
+                    x = (opcode & 0x0F00) >> 8;
+                    y = (opcode & 0x00F0) >> 4;
+                    V[0xF] = (V[x] < V[y]) ? 1 : 0;
+                    V[x] = V[y] - V[x];
+                    pc += 2;
+                    break;
+
+                case 0x000E: // 8xyE - SHL Vx {, Vy}
+                    x = (opcode & 0x0F00) >> 8;
+                    V[0xF] = V[x] >> 7;
+                    V[x] <<= 1;
+                    pc += 2;
+                    break;
+
+                default:
+                    pc += 2;
+                    break;
+                }
+                break;
+
+            case 0x9000: // 9xy0 - SNE Vx, Vy
+                if ((opcode & 0xF) == 0x0) {
+                    x = (opcode & 0x0F00) >> 8;
+                    y = (opcode & 0x00F0) >> 4;
+                    pc += (V[x] != V[y]) ? 4 : 2;
+                }
+                break;
+
+            case 0xA000: // Annn - LD I, addr
+                addr = opcode & 0x0FFF;
+                I = addr;
+                pc += 2;
+                break;
+
+            case 0xB000: // Bnnn - JP V0, addr
+                addr = opcode & 0x0FFF;
+                pc = V[0x0] + addr;
+                break;
+
+            case 0xC000: // Cxkk - RND Vx, byte
+                x = (opcode & 0x0F00) >> 8;
+                kk = opcode & 0x00FF;
+                V[x] = dist(rng) & kk;
+                pc += 2;
+                break;
+
+            case 0xD000: // Dxyn - DRW Vx, Vy, nibble
+                x = (opcode & 0x0F00) >> 8;
+                y = (opcode & 0x00F0) >> 4;
+                n = opcode & 0x000F;
+
+                // display n-byte sprite starting at memory location I at
+                // (Vx,Vy) set VF = collision.
+
+                break;
+
+            case 0xE000:
+                switch (opcode & 0x00FF) {
+                case 0x009E: // Ex9E - SKP Vx
+                    x = (opcode & 0x0F00) >> 8;
+                    pc += keypad[(V[x] & 0x0F)] ? 4 : 2;
+                    break;
+
+                case 0x00A1: // ExA1 - SKNP Vx
+                    x = (opcode & 0x0F00) >> 8;
+                    pc += keypad[(V[x] & 0x0F)] ? 2 : 4;
+                    break;
+
+                default:
+                    pc += 2;
+                    break;
+                }
+                break;
+
+            case 0xF000:
+                switch (opcode & 0x00FF) {
+                case 0x0007: // Fx07 - LD Vx, DT
+                    break;
+
+                case 0x000A: // Fx0A - LD Vx, K
+                    break;
+
+                case 0x0015: // Fx15 - LD DT, Vx
+                    break;
+
+                case 0x0018: // Fx18 - LD ST, Vx
+                    break;
+
+                case 0x001E: // Fx1E - ADD I, Vx
+                    break;
+
+                case 0x0029: // Fx29 - LD F, Vx
+                    break;
+
+                case 0x0033: // Fx33 - LD B, Vx
+                    break;
+
+                case 0x0055: // Fx55 - LD [I], Vx
+                    break;
+
+                case 0x0065: // Fx65 - LD Vx, [I]
+                    break;
+
+                default:
+                    pc += 2;
+                    break;
+                }
             }
         }
     }
